8ddd284befbd44cf159d9eec19bde6d8d73041be
Synthmax <max@synthmax.dk>
2011-12-13 22:51:29 +0100

    redcarpet update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,15 +7,15 @@ module ApplicationHelper
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}   
   end
   
-  def markdown(text)
-      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-                                          :autolink => true, 
-                                          :space_after_headers => true)
-      markdown.render(text).html_safe  
-     
-  #  options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-  #  Redcarpet.new(text, *options).to_html.html_safe
-  end
+#  def markdown(text)
+#      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+#                                          :autolink => true, 
+#                                          :space_after_headers => true)
+#      markdown.render(text).html_safe  
+#     
+#  #  options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
+#  #  Redcarpet.new(text, *options).to_html.html_safe
+#  end
   
   def can_edit?
     user_signed_in? && current_user.admin_or_super?
@@ -24,5 +24,21 @@ module ApplicationHelper
   def grid_is_on?
     Rails.env == 'development' && (user_signed_in? && current_user.grid?)
   end
+  
+  
+  
+  
+  
+  def markdown(text, *renderer)      
+    redcarpet = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(:hard_wrap => true, 
+    :gh_blockcode => true, 
+    :safe_links_only => true, 
+    :filter_html => true))
+    redcarpet.render(text).html_safe
+  end
+
+
+  
+  
   
 end
