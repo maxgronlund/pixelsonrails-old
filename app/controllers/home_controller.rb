9a532e7c4a2cbfb9d820b8502c2be6a9a7839394
Synthmax <max@synthmax.dk>
2012-01-02 23:26:14 +0100

    sorting order on frontpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
     @menu = 'home'
     session[:go_to_after_edit] = root_path
     @welcome = TextContent.welcome
-    @images = GalleryImage.order('sorting asc')#.limit(3)
-    @thumbs = CaseStudy.order('sorting asc').limit(30)
+    @images = GalleryImage.order('sorting desc')#.limit(3)
+    @thumbs = CaseStudy.order('sorting desc').limit(30)
     
     #@gallery_images = GalleryImage.order('sorting asc').limit(10)
   end
